c19101d078c92d0527a787ea82b443ea8c6a86cf
Juan Picado <juanpicado19@gmail.com>
2023-02-11 17:41:58 +0100

    fix copy file

--- tools/cli-commons/src/yarn-modern-utils.ts
+++ tools/cli-commons/src/yarn-modern-utils.ts
@@ -8,7 +8,7 @@ import { Dependencies } from '@verdaccio/types';
 import { createTempFolder, getPackageJSON, getREADME } from './utils';
 
 const fsp = fs.promises ? fs.promises : require('fs/promises');
-const { cp, writeFile, copyFile } = fsp;
+const { writeFile, copyFile } = fsp;
 
 export function createYamlConfig(registry: string, token?: string) {
   const defaultYaml: any = {
@@ -52,6 +52,6 @@ export async function prepareYarnModernProject(
     getPackageJSON(packageName, version, dependencies, devDependencies)
   );
   await writeFile(join(tempFolder, 'README.md'), getREADME(packageName));
-  await copyFile(yarnBinPath, join(tempFolder, '.yarn/releases/yarn.js'), { dereference: true });
+  await copyFile(yarnBinPath, join(tempFolder, '.yarn/releases/yarn.js'));
   return { tempFolder };
 }
