420152fab31f8837951975295bb49ac14c72ef07
Juan Picado <juanpicado19@gmail.com>
2025-03-30 11:13:16 +0200

    test reports

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    reporters: ['default', 'json'], 
+    outputFile: { json: 'vitest-report.json' }, 
     coverage: {
       exclude: ['./build', 'test'],
     },
